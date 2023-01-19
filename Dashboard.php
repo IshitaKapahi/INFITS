02f2c9d3369fb0a5286755aeca4c13c07dca524d
Ishita Kapahi <ishitakapahiofficial@gmail.com>
2023-01-19 20:05:33 +0530

    first commit

--- Dashboard.php
+++ Dashboard.php
@@ -137,10 +137,81 @@ include('event_calender.php');
                 <span><a href="" id="values" style="margin-left:3rem">0.53/2 kg</a></span>
                 <span><a href="" id="values" style=" margin-left:2rem">122/300 kcal</a></span>
             </div>
+
+    </div>  
+    
+    
+
+
+    <!----------------------------MOBILE VIEW OF CLIENT PROGRESSS-------------------->
+
+    <div class="mobileview_clientprogress">
+
+
+
+                <div class="mob_wrapper1" >
+                    <span><a href="" style=" color:black;font-weight:500; border:none; margin-top:1rem;background-color:white; margin-left:1rem"><span><img src="images/ronald.jpg" style="width:2rem;border-radius:1rem"> Ronald Richards</span></a></span>
+                    <div class="row1" style="display:flex ; gap:2rem ">
+                            <div class="steps">
+                                <div class="symbols">
+                                <div style="color:#F6A682"><span class="material-symbols-outlined">footprint</span></div><div style="margin-top:0.2rem; font-weight:500"><span>Steps</span></div></div>
+                                <span style="font-size:0.9rem;color:#454545">5256/6000</span>
+                            </div>
+                            <div class="steps">
+                                <div class="symbols">
+                                <div style="color:#EF80B2"><span class="material-symbols-outlined">monitor_heart</span></div><div style="margin-top:0.2rem; font-weight:500"><span>Heart Rate</span></div></div>
+                                <span style="font-size:0.9rem;color:#454545">123 bpm</span>
+                            </div>
+                    </div>
+
+                    <div class="row2" style="display:flex ; gap:2rem">
+                            <div class="steps">
+                                <div class="symbols">
+                                <div style="color:#8FAFF3"><span class="material-symbols-outlined">local_drink</span></div><div style="margin-top:0.2rem; font-weight:500"><span>Water</span></div></div>
+                                <span style="font-size:0.9rem;color:#454545">2/3 ltrs</span>
+                            </div>
+                            <div class="steps">
+                                <div class="symbols">
+                                <div style="color:#7550E2"><span class="material-symbols-outlined">bedtime</span></div><div style="margin-top:0.2rem; font-weight:500"><span>Sleep</span></div></div>
+                                <span style="font-size:0.9rem;color:#454545">7/8 hrs.</span>
+                            </div>
+                    </div>
+
+                    <div class="row3" style="display:flex ; gap:2rem">
+                            <div class="steps">
+                                <div class="symbols">
+                                <div style="color:#788F96"><span class="material-symbols-outlined">weight</span></div><div style="margin-top:0.2rem; font-weight:500"><span>Weight</span></div></div>
+                                <span style="font-size:0.9rem;color:#454545">0.53/2 kg</span>
+                            </div>
+                            <div class="steps">
+                                <div class="symbols">
+                                <div style="color:#E388A0"><span class="material-symbols-outlined">timer_10_alt_1</span></div><div style="margin-top:0.2rem; font-weight:500"><span>Calories</span></div></div>
+                                <span style="font-size:0.9rem;color:#454545">122/300 kcal</span>
+                            </div>
+                    </div>
+
+
+
+    
         </div>
 
 
 
+
+
+
+
+</div>
+
+
+
+
+
+
+
+             
+
+<!------------------------------------------------------------------------------------------------------------------------------------->
         <div class="dashboard_container5">
 
 
@@ -174,13 +245,38 @@ include('event_calender.php');
             <div class="container5_wrapper2">
                 <div class="tasklist">
                     <p style="font-size:1.3rem ; font-weight:600">Messages</p>
-                    <span><a href="" style="background-color:white; color:#717171 ; border:none" >View All</a></span> 
+                
+                    <span><a href="" style="background-color:white; color:#717171 ; border:none" >View All</a></span>
                 </div>
+                    <div class="meassages">
+                       <div class="tasklist_wrapper1">
+                            <div><img src="images/ronald.jpg" style="width:2rem; background-color:#f8f6f6;border-radius:0.5rem"></div>
+                        </div>
+                        <div class="tasklist_wrapper2">
+                            <span style="font-weight:600">Rick Owens</span>
+                            <span style="margin-botton:4rem; color:#717171">Its Awesome</span>
+                            
+                        </div>
+                        
+                    </div> 
+
+
+            
                 
             </div>
          
         </div>
 
+       
+
+
+
+
+
+
+
+            
+        
 
     </div>
     
