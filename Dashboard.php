af99abed17df330ec27dba11303f4fd9e0d5eaf4
Ishita Kapahi <ishitakapahiofficial@gmail.com>
2023-01-20 13:41:10 +0530

    first commit

--- Dashboard.php
+++ Dashboard.php
@@ -8,9 +8,278 @@ include('event_calender.php');
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Dashboard</title>
-    <link rel="stylesheet" href="Dashboard.css">
+    
     <link href='https://fonts.googleapis.com/css?family=Poppins' rel='stylesheet'>
     <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@48,400,0,0" />
+    <style>
+    .dashboard{
+    margin-top: 2rem;
+    margin-left: 18rem;
+    font-family: poppins;
+    display: flex;
+    flex-direction: column;
+    gap: 1rem;
+    margin-bottom: 1rem; 
+}
+.dashboard_comtainer1{
+    display: flex;
+    gap: 60rem;
+}
+#btn1{
+    border: none;
+    background-color: #0177FD;
+    color: white;
+    padding: 0.4rem;
+    border-radius: 100%;
+    font-size: 1.8rem;
+    padding-left: 1.1rem;
+    padding-right: 1.1rem;
+}
+.dashboard_container2{
+    display: flex;
+    gap: 2rem;
+   
+}
+.container2_wrapper{
+    width: 13rem;
+    height: 10rem;
+    background-color: #7282FB;
+    border-radius: 0.8rem;
+    color: white;
+}
+#box1{
+    background-color: #def9df !important;
+    color: black !important;
+}
+#box2{
+    background-color: #f9cdca !important;
+    color: black !important;
+}
+#box3{
+    background-color: #c5ccff !important;
+    color: black !important;
+}
+.container2_upper{
+    padding: 1rem;
+    font-size: 0.9rem;
+    display: flex;
+    gap: 4.3rem;
+    font-weight: 600;
+}
+.container2_middle{
+    margin-top: 0.5rem;
+    text-align: center;
+    font-weight: 600;
+}
+#btn2{
+    border: none;
+    color: #7282FB;
+    background-color: white;
+    border-radius: 1.5rem;
+    font-size: smaller;
+    padding: 0.3rem;
+    padding-left: 1.2rem;
+    padding-right: 1.2rem;
+    margin-left: 2.7rem;
+    box-shadow: 1px 1px 2px 1px #3a4ae0;
+
+}
+.btn3{
+    border: #88C989 !important;
+    color: #88C989 !important;
+    box-shadow: 1px 1px 2px 1px #88C989 !important;
+
+}
+.btn4{
+    border:  #fba39d !important;
+    color:  #fba39d !important;
+    box-shadow: 1px 1px 2px 1px  #fba39d !important;
+
+}
+.btn5{
+    border: #7282FB !important;
+    color: #7282FB !important;
+    box-shadow: 1px 1px 2px 1px #7282FB !important;
+
+}
+#details{
+    color: #717171;
+    border: none;
+    background-color: white;
+
+}
+.details{
+    margin-left: 45rem;
+    border:none;
+}
+.dashboard_container3{
+    display: flex;
+}
+.dashboard_container4{
+    margin-left: 3rem;
+}
+.container4_wrapper1{
+    display: flex;
+    gap: 2rem;
+    font-weight: 500;
+    margin-left: 20rem;
+    margin-top: 1rem;
+}
+.material-symbols-outlined{
+    margin-top: 0.1rem;
+    margin-right: 0.3rem;
+}
+.symbols{
+    display: flex;
+}
+
+.container4_wrapper2{
+    display: flex;
+    background-color: #f8f6f6;
+    border: 1px solid #e4e1e1;
+    width:fit-content;
+    padding: 0.5rem;
+    border-radius: 0.5rem;
+    margin-top: 1rem;
+
+    
+}
+#values{
+    margin-top: 2rem;
+    background-color:#f8f6f6;
+    color:#454545;
+    font-weight:300;
+    border:none;
+}
+.dashboard_container5{
+    display: flex;
+    gap: 14rem;
+    margin-top: 1rem;
+}
+.tasklist{
+    display: flex;
+    gap: 13rem;
+}
+.list_tasklist{
+    padding: 1rem;
+}
+#btn6{
+    border: none;
+    background-color: white;
+    padding: 0.5rem;
+    padding-left: 4rem;
+    padding-right: 4rem;
+    font-weight: 600;
+    border-radius: 0.6rem;
+}
+#btn6:hover{
+    background-color: #7282FB;
+    color:white;
+    border-radius: 0.5rem;
+
+}
+.list_tasklist_container{
+    display: flex;
+    gap: 1rem;
+    margin: 1.5rem;
+    padding: 0.5rem;
+    background-color: white;
+    border-radius: 0.6rem;
+    
+    
+}
+#tasklist{
+    display: none;
+}
+
+.list_tasklist{
+    width: auto;
+    background-color: #f8f6f6;
+    border: 1px solid  #e4e1e1;
+    border-radius: 0.6rem;
+}
+
+.tasklist_wrapper2{
+    display: flex;
+    flex-direction: column;
+    font-size: 0.7em;
+}
+.tasklist_wrapper3{
+    margin-left: 6rem;
+}
+.meassages{
+    display: flex;
+    gap: 1rem;
+    padding: 0.5rem;
+    border-radius: 0.6rem;
+    background-color: #f8f6f6;
+    border: 1px solid  #e4e1e1;
+
+}
+
+.mobileview_clientprogress{
+    
+    display: none;
+}
+.steps{
+  margin-top: 1rem;
+}
+
+@media screen and (max-width: 720px) {
+    .dashboard_comtainer1{
+        display: flex;
+        gap:8rem;
+    }
+    .dashboard{
+        margin-left: 2rem;
+    }
+    .dashboard_container2{
+        display: flex;
+        flex-direction: column;
+        gap: 1rem;
+        margin-left: 3rem;
+    }
+    .dashboard_container3{
+        display: flex;
+        flex-direction: column;
+    }
+    .details{
+        margin-left:0px;
+    }
+    .dashboard_container5{
+        display: flex;
+        flex-direction: column;
+        gap: 1rem;
+    }
+    .tasklist{
+        display: flex;
+        gap: 8rem;
+    }
+    .dashboard_container4{
+        display: none;
+    }
+    #btn6{
+        padding-left: 2rem;
+        padding-right: 2rem
+    }
+    .tasklist_wrapper3{
+        margin-left: 0.5rem;
+    }
+   
+    .mobileview_clientprogress{
+        display: flex;
+        flex-direction: column;
+        gap: 1rem;
+        box-shadow: 0 4px 7px rgba(0,0,0,0.12);
+        border-radius: 0.7rem;
+        padding: 1rem;
+        width: 260px;
+        height: 283px;
+        margin-left: 2rem;
+    }
+   
+}
+    </style>
 </head>
 <body>
     <div class="dashboard">
